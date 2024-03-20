e704f3a2d85332fee0e2b4050bb5560e146113a0
Alberto <alberto.martin1535@gmail.com>
2024-03-20 10:34:03 +0100

    Add improvements

--- services/index.js
+++ services/index.js
@@ -1,4 +1,33 @@
+import sigV4Client from '../utils/sigV4Client'
+import { get } from 'aws-amplify/api';
+
+
+import { fetchAuthSession } from 'aws-amplify/auth'
+
+
 const server = "https://sgsldh5134.execute-api.eu-west-1.amazonaws.com";
+
+export const getArticle = async() =>{
+  try {
+    const idToken = (await fetchAuthSession()).tokens?.idToken?.toString();
+
+    const restOperation = get({ 
+      apiName: 'ascpi',
+      path: '/articles',
+      options: {
+        headers: {
+          // @ts-ignore: Unreachable code error
+          Authorization: idToken,
+          'X-Amz-Date': new Date().getTime().toString()
+        }
+      }
+    });
+    const response = await restOperation.response;
+    console.log('GET call succeeded: ', response);
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 
 export const getArticles = async (token) =>{
@@ -9,3 +38,42 @@ export const getArticles = async (token) =>{
 
     fetch(`${server}`, {method:"GET", headers: myHeaders});
 }
+
+
+/*export const getArticle = async() =>{
+  try {
+    const session = (await fetchAuthSession());
+    console.log(session)
+
+    const idToken = session.tokens?.idToken?.toString();
+
+
+    const signedRequest = sigV4Client
+    .newClient({
+      accessKey: session.credentials.accessKeyId,
+      secretKey: session.credentials.secretAccessKey,
+      sessionToken: idToken,
+      region: "eu-west-1",
+      endpoint: server
+    })
+    .signRequest({
+      method: "GET",
+      path: `${server}/articles`,
+      headers: {Authorization: "123"},
+      queryParams: "lol=123",
+      body: {a: "123"}
+    });
+
+    const restOperation = get({ 
+      apiName: 'ascpi',
+      path: '/articles',
+      options: {
+        headers: signedRequest
+      }
+    });
+    const response = await restOperation.response;
+    console.log('GET call succeeded: ', response);
+  } catch (e) {
+    console.log(e)
+  }
+}*/
